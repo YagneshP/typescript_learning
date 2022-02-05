deccf87a5eda26440deff2db6d62b1451b75419a
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-02-05 13:28:21 -0500

    how to define object type

--- basic.ts
+++ basic.ts
@@ -5,3 +5,18 @@ let isTrue: boolean = true;
 userName += 'Parekh';
 
 console.log(userName);
+
+//array type
+const arr: string[]  = userName.split(' ');
+// you can specify array type like 'Array<number>' this also
+const numArr: Array<number> = [1,2];
+
+//object types
+
+const myPerson: {
+  firstName: string,
+  lastName: string
+} = {
+  firstName: 'Yagnesh',
+  lastName: 'Parekh'
+};
