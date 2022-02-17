17edcd39721e91ac0aae53c4f6da3c352ad7df28
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-02-17 16:09:48 -0500

    type-assertion

---
+++ type_assertion.ts
@@ -0,0 +1,8 @@
+// sometime you know the type of value 
+
+const myCanvas = document.getElementById("canvas") as HTMLCanvasElement;
+
+// another syntaxt
+
+const yourCanvas = <HTMLCanvasElement>document.getElementById("canvas");
+
